b49253c271beb3904cd152e306e79719c3b6256d
ring-wdr <akswnd55@gmail.com>
2023-11-02 23:09:04 +0900

    docs: README 생성

---
+++ global.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  class Random {
+    static pickUniqueNumbersInRange(
+      startInclusive: number,
+      endInclusive: number,
+      count: number
+    ): number[];
+  }
+  class Console {
+    static readLineAsync(query: string): Promise<string>;
+    static print(message): void;
+  }
+}
