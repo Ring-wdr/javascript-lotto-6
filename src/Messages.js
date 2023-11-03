704afbbebca0eb8f42c0f860281d484cb114c791
ring-wdr <akswnd55@gmail.com>
2023-11-03 19:39:35 +0900

    feat: 사용할 메시지 파일 작성

---
+++ src/Messages.js
@@ -0,0 +1,16 @@
+const MESSEGE = Object.freeze({
+  ERROR_NUMBER: "[ERROR] 숫자가 잘못된 형식입니다.",
+  ERROR_LOTTO_NUMBER: "[ERROR] 쉼표로 구분된 숫자 형식이 아닙니다.",
+  INPUT_BUY_AMT: "구입금액을 입력해 주세요.",
+  SHOW_BUY_COUNT: "개를 구매했습니다.",
+  INPUT_WIN_NUM: "당첨 번호를 입력해 주세요.",
+  INPUT_BONUS_NUM: "보너스 번호를 입력해 주세요.",
+  SUMMARY: "당첨 통계",
+  NUMBER_FIT: "개 일치",
+  COUNT: "개",
+  WON: "원",
+  BONUS_FIT: "보너스 볼 일치",
+  TOTAL_BENEFIT: "총 수익률은",
+  IS: "입니다.",
+});
+export default MESSEGE;
