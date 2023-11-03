1b093585d37bdceaa54f30b924708d74f1314194
ring-wdr <akswnd55@gmail.com>
2023-11-04 04:23:34 +0900

    feat: 메세지 모듈화

--- src/Messages.js
+++ src/Messages.js
@@ -1,6 +1,8 @@
 const MESSEGE = Object.freeze({
   ERROR_NUMBER: "[ERROR] 숫자가 잘못된 형식입니다.",
   ERROR_LOTTO_NUMBER: "[ERROR] 쉼표로 구분된 숫자 형식이 아닙니다.",
+  ERROR_LOTTO_SIX: "[ERROR] 로또 번호는 6개여야 합니다.",
+  ERROR_LOTTO_DUPL:"[ERROR] 로또 번호는 중복된 숫자가 없어야 합니다.",
   INPUT_BUY_AMT: "구입금액을 입력해 주세요.",
   SHOW_BUY_COUNT: "개를 구매했습니다.",
   INPUT_WIN_NUM: "당첨 번호를 입력해 주세요.",
