1199120b7708bb3fb964d1fcbb1e607b17eccdc8
ring-wdr <akswnd55@gmail.com>
2023-11-04 04:26:05 +0900

    refactor: added getter to use private field

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,32 @@
+import MESSEGE from "./Messages";
+
 class Lotto {
+  /** @type {number[]} */
   #numbers;
 
+  /** @param {number[]} numbers*/
   constructor(numbers) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
 
+  /** @param {number[]} numbers  */
   #validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(MESSEGE.ERROR_LOTTO_SIX);
+    }
+    if (
+      numbers.some((num) => numbers.filter((num2) => num === num2).length > 1)
+    ) {
+      throw new Error(MESSEGE.ERROR_LOTTO_DUPL);
     }
   }
 
   // TODO: 추가 기능 구현
+  /** @readonly */
+  get getLottoNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
